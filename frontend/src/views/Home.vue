6827e38f75fcf974da203349e7d7f39e5b2377c5
Baris <baslan374@gmail.com>
2022-07-10 20:59:43 +0300

    Initial commit

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="post-card">
+    <h1>This is a post card </h1>
+    <p> This is a post card  This is a post card. This is a post cardThis is a post cardThis is a post card</p>
+  </div>
+</template>
+
+<script>
+import API from '../api'
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      posts: []
+    }
+  },
+   async created() {
+    this.posts = await API.getAllPosts()
+  }
+}
+</script>
+<style scoped>
+.post-card {
+  border: 5px solid red;
+}
+</style>
