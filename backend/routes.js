f86f2e4fff4bbf5834c1f5eeab4e7340d73fc167
Baris <baslan374@gmail.com>
2022-07-11 20:50:43 +0300

    Remove comment

--- backend/routes.js
+++ backend/routes.js
@@ -2,11 +2,6 @@ const express = require('express');
 const router = express.Router();
 const API = require('./api');
 
-/*router.get('/' , (req,res) => {
-    res.send('Hello Baris!');
-}
-)*/
-
 router.get('/' , API.fetchAllPosts);
 router.get('/:id' , API.fetchPostById);
 router.post('/' , API.createPost);
