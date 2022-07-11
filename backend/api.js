db54dfd6977f6dd8f0183092944baf2e81e91854
Baris <baslan374@gmail.com>
2022-07-11 21:54:31 +0300

    Add fetchPostById function

--- backend/api.js
+++ backend/api.js
@@ -13,13 +13,18 @@ module.exports = class API {
     }
 
     static async fetchPostById(req,res) {
-        res.send('Hello, I am API with ID!');
+        const id = req.params.id;
+        try {
+            const post = await Posts.findById(id)
+            res.status(200).json(post);
+        }   catch(err) {
+            res.status(404).json({message: err.message})
+        }
     }
 
     static async createPost(req,res) {
         const post = req.body;
-        const image = req.filename;
-        post.image = image
+        
         try {
             await Posts.create(post);
             res.status(201).json({message:'Post created!'});
