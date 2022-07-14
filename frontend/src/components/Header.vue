2714756a701b63039248a234756211e59c6039dc
Baris <baslan374@gmail.com>
2022-07-14 20:02:10 +0300

    Create Header component

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,71 @@
+<template>
+<div class="header">
+  <img class="logo" src="../assets/ohMyBlog-logos_black.png"/>
+  <div class="top-right">
+    <router-link style="text-decoration: none" to="/"><div class="header-btn">Home</div></router-link>
+    <router-link style="text-decoration: none" to="/about"><div class="header-btn">About</div></router-link>
+  </div>    
+  </div>
+    <h1 class="welcome">Welcome to ohMyBlog!</h1>
+    <button class="create">Create Post</button>
+
+</template>
+
+<script>
+export default {
+    name:'Header',
+}
+
+</script>
+
+<style>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  position: relative;
+  background-image: url(../assets/photo-1499750310107-5fef28a66643.avif);
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  height:45rem;
+  margin-bottom: 8rem;
+  opacity: .8;
+}
+
+.top-right{
+  display: flex;
+  padding: 2rem 3rem;
+  
+}
+.header-btn {
+  color:rgba(255, 177, 0, .9);
+  padding: 0 1rem;
+  font-size:1.4rem;
+}
+
+.logo {
+  width:14rem;
+  padding-top:.8rem;
+}
+
+.welcome {
+  color:rgba(255, 177, 0, .9);
+  position:absolute;
+  top:27rem;
+  left:2.5rem;
+  font-size:4rem;
+}
+
+.create {
+  position:absolute;
+  top:32rem;
+  left:3rem;
+  border:none;
+  border-radius: 100px;
+  width:11rem;
+  padding:.5rem;
+  color:white;
+  background-color:	#de5e02;
+  font-size:1.5rem;
+}
+</style>
