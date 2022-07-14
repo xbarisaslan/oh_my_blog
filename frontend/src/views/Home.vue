2e33c565d99ef07fd9fe4c6c9f83441ce59280d0
Baris <baslan374@gmail.com>
2022-07-14 20:03:52 +0300

    Move header code to Header component

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,14 +1,23 @@
 <template>
-  <div class="post-card" v-for="post in posts">
-    
+<Header />
+  <div class="posts">
+  <div class="blog-post" v-for="post in posts" :key="post._id" >
+    <h3>{{post.title}}</h3>
+    <p>{{post.content}}</p>
+    <p>{{post.author}}</p>
+  </div>
   </div>
 </template>
 
 <script>
 import API from '../api'
+import Header from '../components/Header.vue'
 
 export default {
   name: 'Home',
+  components: {
+    Header,
+  },
   data() {
     return {
       posts: []
@@ -20,7 +29,16 @@ export default {
 }
 </script>
 <style scoped>
-.post-card {
-  border: 5px solid red;
+
+.posts{
+  display: grid;
+  grid-template-columns: repeat(auto-fill , minmax(500px,1fr));
+  gap:4rem 2rem;
+  padding:3rem;
 }
+.blog-post {
+  border: 2px solid red;
+  background-color: rgba(245, 245, 245, 1);
+}
+
 </style>
