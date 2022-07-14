d6a3ce998d5b6a98f296f7911090290704fe565c
Baris <baslan374@gmail.com>
2022-07-14 21:22:36 +0300

    Create Posts.vue

---
+++ frontend/src/components/Posts.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="posts">
+  <div class="blog-post" v-for="post in posts" :key="post._id" >
+    <h3>{{post.title}}</h3>
+    <p>{{post.content}}</p>
+    <p>{{post.author}}</p>
+  </div>
+</div>
+
+</template>
+
+<script>
+import API from '../api'
+export default {
+    name: 'Posts',
+    data() {
+    return {
+      posts: []
+    }
+  },
+   async created() {
+    this.posts = await API.getAllPosts()
+  }
+}
+</script>
+
+<style scoped>
+
+.posts{
+  display: grid;
+  grid-template-columns: repeat(auto-fill , minmax(500px,1fr));
+  gap:4rem 2rem;
+  padding:3rem;
+}
+
+.blog-post {
+  border: 2px solid red;
+  background-color: rgba(245, 245, 245, 1);
+}
+</style>
