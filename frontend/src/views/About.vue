7a922c01274f5abcba9e87dc505aecddc90f62b4
Baris <baslan374@gmail.com>
2022-07-21 15:46:44 +0300

    Import AboutPage component

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,18 +1,18 @@
 <template>
   <NavBar />
+  <AboutPage />
 </template>
 
 <script>
 import NavBar from '../components/NavBar'
+import AboutPage from '../components/AboutPage.vue'
 
 export default {
   name: 'About',
   components: {
     NavBar,
+    AboutPage,
   }
 }
 </script>
 
-<style>
-
-</style>
