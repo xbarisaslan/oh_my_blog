4ab8179c3848ff53d2faf2da7e80c8a6bb6bdeee
Baris <baslan374@gmail.com>
2022-07-21 15:46:27 +0300

    Create AboutPage component

---
+++ frontend/src/components/AboutPage.vue
@@ -0,0 +1,25 @@
+<template>
+ <div class="about">
+    <div class="text">
+    <h1>About OhMyBlog</h1>
+    <p>Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.</p>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name:'AboutPage',
+}
+</script>
+
+<style scoped>
+.about {
+  display: flex;
+  padding-top: 2rem;
+  padding-left: 1rem;
+}
+.text {
+    line-height: 3rem;
+}
+</style>
