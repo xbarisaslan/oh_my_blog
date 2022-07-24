042cbe5651fa6e27c100ec04b318ca8d6d56b670
Baris <baslan374@gmail.com>
2022-07-24 11:03:15 +0300

    Add router-link to /post/:id

--- frontend/src/components/Posts.vue
+++ frontend/src/components/Posts.vue
@@ -1,8 +1,9 @@
 <template>
 <div class="posts">
-  <div class="blog-post" v-for="post in posts" :key="post._id" >
+  <div class="blog-post" v-for="post in posts" :key="post._id">
     <h3>{{post.title}}</h3>
-    <p>{{post.content}}</p>
+    <p class="content">{{post.content.substring(0,100)}}...</p>
+    <router-link style="text-decoration:none;" :to="'/post/' + post._id"><h4 class="redirect">Read more</h4></router-link>
     <p style="font-weight:bold;">Author: {{post.author}}</p>
   </div>
 </div>
@@ -11,6 +12,7 @@
 
 <script>
 import API from '../api'
+
 export default {
   name: 'Posts',
   data() {
@@ -18,28 +20,40 @@ export default {
       posts: []
     }
   },
-   async created() {
+  async created() {
     this.posts = await API.getAllPosts()
-  }
+  },
+ 
 }
 </script>
 
 <style scoped>
 h3,p {
   padding:1rem;
-  line-height: .5rem;
+  line-height: 1.5rem;
+}
+.content {
+  height:2rem;
 }
 .posts{
   display: grid;
   grid-template-columns: repeat(auto-fill , minmax(500px,1fr));
   gap:4rem 2rem;
   padding: 2rem;
+  background-color: rgba(205, 208, 202, .65)
 }
 
 .blog-post {
-  background-color: rgba(248, 248, 248, .75);
+  background-color: rgba(238, 238, 238, .75);
   border: 1px solid rgba(248, 248, 248, 1);
   border-radius: 10px;
   padding-left: 1rem;
+  word-wrap: break-word;
+}
+
+.redirect {
+  color:rgba(246, 155, 107, 1);
+  text-align: end;
+  padding-right: 1rem;
 }
 </style>
