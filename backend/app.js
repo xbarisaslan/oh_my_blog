5836753b5600694fdac22e3825c322643db36ab0
Baris <baslan374@gmail.com>
2022-07-24 11:01:57 +0300

    Add bodyParser

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser')
 
 require('dotenv').config();
 const port = process.env.PORT;
@@ -9,7 +10,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+ })); 
+app.use(bodyParser.json());
 app.use("/api/post" , require("./routes"));
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
