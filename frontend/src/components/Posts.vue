a17455817032aa4d137126f62b8cb64d17e06c85
Baris <baslan374@gmail.com>
2022-07-26 11:34:40 +0300

    Remove class from h4 tag

--- frontend/src/components/Posts.vue
+++ frontend/src/components/Posts.vue
@@ -3,7 +3,7 @@
   <div class="blog-post" v-for="post in posts" :key="post._id">
     <h3>{{post.title}}</h3>
     <p class="content">{{post.content.substring(0,100)}}...</p>
-    <router-link style="text-decoration:none;" :to="'/post/' + post._id"><h4 class="redirect">Read more</h4></router-link>
+    <router-link style="text-decoration:none;" :to="'/post/' + post._id"><h4>Read more</h4></router-link>
     <p style="font-weight:bold;">Author: {{post.author}}</p>
   </div>
 </div>
@@ -32,6 +32,12 @@ h3,p {
   padding:1rem;
   line-height: 1.5rem;
 }
+
+h4 {
+  color:rgba(246, 155, 107, 1);
+  text-align: end;
+  padding-right: 1rem;
+}
 .content {
   height:2rem;
 }
@@ -51,9 +57,4 @@ h3,p {
   word-wrap: break-word;
 }
 
-.redirect {
-  color:rgba(246, 155, 107, 1);
-  text-align: end;
-  padding-right: 1rem;
-}
 </style>
