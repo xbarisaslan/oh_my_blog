7f7e962e6da40c086d0eb6e5920188e650ef811c
Baris <baslan374@gmail.com>
2022-07-27 11:03:42 +0300

    Create Post.vue

---
+++ frontend/src/views/Post.vue
@@ -0,0 +1,30 @@
+<template>
+<NavBar />
+<div class="blog-post">
+    <h3>{{post.title}}</h3>
+    <p>{{post.content}}</p>
+    <p style="font-weight:bold;">Author: {{post.author}}</p>
+</div>
+</template>
+
+<script>
+import API from '../api'
+import NavBar from '../components/NavBar'
+
+export default {
+    name: 'Post',
+    components: {
+        NavBar,
+    },
+    data() {
+    return {
+      post: {}
+    }
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
