c7d6c60d66dc5f17f57b1cd56c8aefb5cd8197d6
Baris <baslan374@gmail.com>
2022-07-29 22:42:19 +0300

    Delete files

--- backend/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const API = require('./api');
-
-router.get('/' , API.fetchAllPosts);
-router.get('/:id' , API.fetchPostById);
-router.post('/' , API.createPost);
-
-module.exports = router;
