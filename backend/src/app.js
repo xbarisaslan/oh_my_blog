67c5b52e8d9e07f6ec59f2f704dacc591a6f2f56
Baris <baslan374@gmail.com>
2022-07-29 22:42:34 +0300

    Move app.js to src folder

---
+++ backend/src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+
+require('dotenv').config();
+const port = process.env.PORT || 5000
+const mongooseConnection = require('./database-connection')
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+ })); 
+app.use(bodyParser.json());
+
+app.use("/api/post" , require('./routes/routes'));
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(__dirname + '/dist/'));
+    app.get('*', (req,res) => res.sendFile(__dirname + '/dist/index.html'));
+}
+
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
